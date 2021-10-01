243904a2386a7269f3ebcba19e9cd10d917f0f61
ANURAG MISHRA <anuragmishra.ofc@gmail.com>
2021-10-02 02:03:24 +0530

    Added the add teask bar and button

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
@@ -14,6 +14,20 @@ export default function App() {
           <Task Text={'Task 2'} />
         </View>
       </View>
+
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writetask}>
+        <TextInput style={styles.input} placeholder={"write a Task"}>
+
+        </TextInput>
+        <TouchableOpacity>
+          <View style={styles.addwrapper}>
+            <Text style={styles.addtext}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
 
     </View>
   );
@@ -35,5 +49,38 @@ const styles = StyleSheet.create({
   },
   items: {
     margin: 30,
+
   },
+  writetask: {
+    position: 'absolute',
+    bottom: 60,
+    width: "100%",
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    width: 250,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: '#C0C0C0',
+
+
+  },
+  addwrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#C0C0C0'
+  },
+  addtext: {},
+
 });
